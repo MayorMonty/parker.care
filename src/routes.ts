f6306497cf9013b7fae3c9008b82052652afaf39
Brendan McGuire <brendan@bren.app>
2021-08-22 19:30:05 -0400

    Add submit page

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,11 @@ import expressWinston from "express-winston"
 import { static as assets } from "express";
 import { promises as fs } from "fs";
 import { join } from "path";
-import { getRandomMessage } from "./database"
+import { addMessage, getRandomMessage } from "./database"
+import formidable from "express-formidable";
+
+// Form parsing
+app.use(formidable());
 
 // Setup logging 
 app.use(expressWinston.logger({
@@ -21,6 +25,38 @@ app.get("/message.json", async (req, res, next) => {
   }
 });
 
+// Serve submission page
+app.get("/submit", async (req, res, next) => {
+    res.sendFile(join(__dirname, "../../assets/submit.html"));
+});
+
+app.post("/submit", async (req, res, next) => {
+
+  if (req.fields) {
+
+    try {
+      const { name, message } = req.fields;
+      await addMessage(message.toString(), name.toString().toUpperCase());
+      res.status(400).send({
+        "status": "ok",
+      });
+
+    } catch (err) {
+      res.status(500).send({
+        "status": "error",
+        "error_message": `Internal Server Error: ${err}`
+      });
+    }
+
+  } else {
+    res.status(400).send({
+      "status": "error",
+      "error_message": "No form data submitted"
+    });
+  }
+
+
+});
 
 // Static Content
 app.use(assets(join(__dirname, "../../assets")));
